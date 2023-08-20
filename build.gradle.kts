1b03090c7e59504deeb418983d977896800aad91
Marcel Lautenbach <mlautenb@gmail.com>
2023-08-20 12:21:06 +0200

    bump dependency versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.8.22"
-    kotlin("plugin.allopen") version "1.8.22"
+    kotlin("jvm") version "1.9.0"
+    kotlin("plugin.allopen") version "1.9.0"
     id("io.quarkus")
     id("com.github.ben-manes.versions") version "0.47.0"
 }
@@ -19,12 +19,12 @@ dependencies {
     implementation("io.quarkus:quarkus-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("io.quarkus:quarkus-arc")
-    implementation("io.kubernetes:client-java:18.0.0")
+    implementation("io.kubernetes:client-java:18.0.1")
 //    implementation("io.kubernetes:client-java-extended:18.0.0")
     implementation("io.quarkus:quarkus-config-yaml")
     implementation("io.quarkus:quarkus-container-image-jib")
-    testImplementation("io.quarkiverse.operatorsdk:quarkus-operator-sdk-bom:6.2.1")
-    implementation("io.quarkiverse.operatorsdk:quarkus-operator-sdk-bundle-generator:6.2.1")
+    testImplementation("io.quarkiverse.operatorsdk:quarkus-operator-sdk-bom:6.3.0")
+    implementation("io.quarkiverse.operatorsdk:quarkus-operator-sdk-bundle-generator:6.3.0")
 }
 
 
