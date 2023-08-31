f0e3b927c87ba02d528e0095767135b8f0437767
Marcel Lautenbach <mlautenb@gmail.com>
2023-08-31 23:26:41 +0200

    fix some logic and test operator-sdk 6.3.1-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.9.0"
-    kotlin("plugin.allopen") version "1.9.0"
+    kotlin("jvm") version "1.9.10"
+    kotlin("plugin.allopen") version "1.9.10"
     id("io.quarkus")
     id("com.github.ben-manes.versions") version "0.47.0"
 }
@@ -14,23 +14,28 @@ val quarkusPlatformGroupId: String by project
 val quarkusPlatformArtifactId: String by project
 val quarkusPlatformVersion: String by project
 
+
 dependencies {
     implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
+    implementation(enforcedPlatform("io.quarkiverse.operatorsdk:quarkus-operator-sdk-bom:6.3.0"))
     implementation("io.quarkus:quarkus-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("io.quarkus:quarkus-arc")
-    implementation("io.kubernetes:client-java:18.0.1")
+//    implementation("io.kubernetes:client-java:18.0.1")
 //    implementation("io.kubernetes:client-java-extended:18.0.0")
     implementation("io.quarkus:quarkus-config-yaml")
     implementation("io.quarkus:quarkus-container-image-jib")
-    testImplementation("io.quarkiverse.operatorsdk:quarkus-operator-sdk-bom:6.3.0")
-    implementation("io.quarkiverse.operatorsdk:quarkus-operator-sdk-bundle-generator:6.3.0")
+
+    implementation("io.quarkiverse.operatorsdk:quarkus-operator-sdk-bundle-generator:6.3.1-SNAPSHOT")
+    implementation("io.quarkiverse.operatorsdk:quarkus-operator-sdk:6.3.1-SNAPSHOT")
+    implementation("io.quarkiverse.operatorsdk:quarkus-operator-sdk-annotations:6.3.1-SNAPSHOT")
+
 }
 
 
 
 group = "com.example"
-version = "1.0-SNAPSHOT"
+version = "1.1.0"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
--- src/main/kotlin/certmanger/ExposedAppReconciler.kt
+++ src/main/kotlin/certmanger/ExposedAppReconciler.kt
@@ -7,26 +7,42 @@ import io.javaoperatorsdk.operator.api.reconciler.Context
 import io.javaoperatorsdk.operator.api.reconciler.ControllerConfiguration
 import io.javaoperatorsdk.operator.api.reconciler.Reconciler
 import io.javaoperatorsdk.operator.api.reconciler.UpdateControl
+import io.quarkiverse.operatorsdk.annotations.RBACRule
+import io.quarkiverse.operatorsdk.annotations.RBACVerbs
 import java.util.logging.Level
 import java.util.logging.Logger
 
+const val CERTIFICATES_K8S_IO_GROUP = "certificates.k8s.io"
+const val ADDITIONAL_UPDATE_RESOURCE = "certificatesigningrequests/approval"
+const val SIGNERS_VERB = "approve"
+const val SIGNERS_RESOURCE = "signers"
+const val SIGNERS_RESOURCE_NAMES = "kubernetes.io/kubelet-serving"
+
 @ControllerConfiguration(name = "server-cert-approve")
+@RBACRule(verbs = [RBACVerbs.UPDATE], apiGroups = [CERTIFICATES_K8S_IO_GROUP], resources = [ADDITIONAL_UPDATE_RESOURCE])
+@RBACRule(verbs = [SIGNERS_VERB], apiGroups = [CERTIFICATES_K8S_IO_GROUP], resources = [SIGNERS_RESOURCE], resourceNames = [SIGNERS_RESOURCE_NAMES])
 class ExposedAppReconciler(private val client: KubernetesClient) : Reconciler<CertificateSigningRequest?> {
+
 
     override fun reconcile(resource: CertificateSigningRequest?, context: Context<CertificateSigningRequest?>?): UpdateControl<CertificateSigningRequest?> {
         val logger = Logger.getLogger(ExposedAppReconciler::class.toString())
 
-        logger.log(Level.INFO, "Found new pending CSR: ${resource?.metadata?.name}")
+        logger.log(Level.INFO, "Incoming CSR: ${resource?.metadata?.name}")
+
         try {
             val r = client.certificates().v1().certificateSigningRequests().resource(resource)
-            r.item().status.conditions.find { it.type != "Approved" } ?: {
-                logger.log(Level.INFO, "Approving ${resource?.metadata?.name}")
+            r.item().status.conditions.find { it.type == "Approved" } ?: also {
+                logger.log(Level.INFO, "Found pending CSR: ${resource?.metadata?.name}")
                 r.approve()
+                logger.log(Level.INFO, "Approved CSR ${resource?.metadata?.name}")
+                return UpdateControl.patchStatus(resource)
             }
         } catch (e: KubernetesClientException) {
+            logger.log(Level.WARNING, e.stackTraceToString())
             logger.log(Level.WARNING, "Server Timeout")
         }
 
-        return UpdateControl.patchStatus(resource)
+        logger.log(Level.INFO, "CSR: ${resource?.metadata?.name} already approved")
+        return UpdateControl.noUpdate()
     }
 }
